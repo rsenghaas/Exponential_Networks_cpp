2c000308368ed07aecf22d5fee14c726eb2710e4
Raphael Senghaas <raphael.senghaas@gmail.com>
2022-09-02 23:09:59 +0200

    New path added

--- util.sh
+++ util.sh
@@ -7,4 +7,5 @@ case "$1" in
     -d | --draw ) python sandbox.py all;;
     -s | --show ) python sandbox.py all; feh -. graphics/test_graphic.png &;;
     -sf | --show_file ) feh -. graphics/"$2".png &;;
+    -g | --git ) git add .; git commit -m "$2"; git push origin main;;
 esac
